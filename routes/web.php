720aad85ab39a85cc47021765e3514f9b06ab672
mYuking <kingyukingyouking@gmail.com>
2024-05-22 13:44:06 +0700

    push eLearning

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\StudentCotroller;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,3 +9,7 @@ Route::get('/', function () {
 });
 
 Route::get('admin/dashboard', [DashboardController::class, 'index']);
+
+// Route untuk menampilkan student 
+Route::get('admin/student', [StudentCotroller::class, 'index']);
+
